53d4ab5c81295a2db8e533a0943b4f2f2416ca04
Noah Berman <noah@noahberman.org>
2017-01-10 11:53:15 -0700

    add adafruit python files

--- Servo_Example.py
+++ Servo_Example.py
@@ -1,12 +1,16 @@
+#!/usr/bin/python
+
+from Adafruit_PWM_Servo_Driver import PWM
 import time
-from Adafruit_Raspberry_Pi_Python_Code/Adafruit_PWM_Servo_Driver import Adafruit_I2C
-from Adafruit_Raspberry_Pi_Python_Code/Adafruit_PWM_Servo_Driver import Adafruit_PWM_Servo_Driver
+
 # ===========================================================================
-# Coffee Maker Bot Script
+# Example Code
 # ===========================================================================
 
 # Initialise the PWM device using the default address
-pwm = PWM(0x40, debug=True)
+pwm = PWM(0x40)
+# Note if you'd like more debug output you can instead run:
+#pwm = PWM(0x40, debug=True)
 
 servoMin = 150  # Min pulse length out of 4096
 servoMax = 600  # Max pulse length out of 4096
@@ -18,20 +22,8 @@ def setServoPulse(channel, pulse):
   pulseLength /= 4096                     # 12 bits of resolution
   print "%d us per bit" % pulseLength
   pulse *= 1000
-                                                            #  1,1           Top
-
-# servoMin = 150  # Min pulse length out of 4096
-# servoMax = 600  # Max pulse length out of 4096
-# 
-# def setServoPulse(channel, pulse):
-#   pulseLength = 1000000                   # 1,000,000 us per second
-#   pulseLength /= 60                       # 60 Hz
-#   print "%d us per period" % pulseLength
-#   pulseLength /= 4096                     # 12 bits of resolution
-#   print "%d us per bit" % pulseLength
-#   pulse *= 1000
-#   pulse /= pulseLength
-#   pwm.setPWM(channel, 0, pulse)
+  pulse /= pulseLength
+  pwm.setPWM(channel, 0, pulse)
 
 pwm.setPWMFreq(60)                        # Set frequency to 60 Hz
 while (True):
@@ -40,3 +32,6 @@ while (True):
   time.sleep(1)
   pwm.setPWM(0, 0, servoMax)
   time.sleep(1)
+
+
+

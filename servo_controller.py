ec0f019b945441755d1211263c3f4ecd24c8afd4
Noah Berman <noah@noahberman.org>
2017-01-10 11:56:03 -0700

    modified script to run once

--- servo_controller.py
+++ servo_controller.py
@@ -18,22 +18,9 @@ def setServoPulse(channel, pulse):
   pulseLength /= 4096                     # 12 bits of resolution
   print "%d us per bit" % pulseLength
   pulse *= 1000
-                                                            #  1,1           Top
 
-# servoMin = 150  # Min pulse length out of 4096
-# servoMax = 600  # Max pulse length out of 4096
-# 
-# def setServoPulse(channel, pulse):
-#   pulseLength = 1000000                   # 1,000,000 us per second
-#   pulseLength /= 60                       # 60 Hz
-#   print "%d us per period" % pulseLength
-#   pulseLength /= 4096                     # 12 bits of resolution
-#   print "%d us per bit" % pulseLength
-#   pulse *= 1000
-#   pulse /= pulseLength
-#   pwm.setPWM(channel, 0, pulse)
+pwm.setPWMFreq(1)                        # Set frequency to 60 Hz
 
-pwm.setPWMFreq(60)                        # Set frequency to 60 Hz
 while (True):
   # Change speed of continuous servo on channel O
   pwm.setPWM(0, 0, servoMin)

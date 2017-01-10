43dbf62a8e592a9695773f82a9a9a7ec330273f4
Noah Berman <noah@noahberman.org>
2017-01-10 11:18:26 -0700

    modify script for alternate system

--- servo.controller.py
+++ servo.controller.py
@@ -1,26 +1,41 @@
-# Servo Control
 import time
-import wiringpi
- 
-# use 'GPIO naming'
-wiringpi.wiringPiSetupGpio()
- 
-# set #18 to be a PWM output
-wiringpi.pinMode(18, wiringpi.GPIO.PWM_OUTPUT)
- 
-# set the PWM mode to milliseconds stype
-wiringpi.pwmSetMode(wiringpi.GPIO.PWM_MODE_MS)
- 
-# divide down clock
-wiringpi.pwmSetClock(192)
-wiringpi.pwmSetRange(2000)
- 
-delay_period = 0.01
- 
-while True:
-        for pulse in range(50, 250, 1):
-                wiringpi.pwmWrite(18, pulse)
-                time.sleep(delay_period)
-        for pulse in range(250, 50, -1):
-                wiringpi.pwmWrite(18, pulse)
-                time.sleep(delay_period)
+
+# ===========================================================================
+# Coffee Maker Bot Script
+# ===========================================================================
+
+# Initialise the PWM device using the default address
+pwm = PWM(0x40, debug=True)
+
+servoMin = 150  # Min pulse length out of 4096
+servoMax = 600  # Max pulse length out of 4096
+
+def setServoPulse(channel, pulse):
+  pulseLength = 1000000                   # 1,000,000 us per second
+  pulseLength /= 60                       # 60 Hz
+  print "%d us per period" % pulseLength
+  pulseLength /= 4096                     # 12 bits of resolution
+  print "%d us per bit" % pulseLength
+  pulse *= 1000
+                                                             1,1           Top
+
+# servoMin = 150  # Min pulse length out of 4096
+# servoMax = 600  # Max pulse length out of 4096
+# 
+# def setServoPulse(channel, pulse):
+#   pulseLength = 1000000                   # 1,000,000 us per second
+#   pulseLength /= 60                       # 60 Hz
+#   print "%d us per period" % pulseLength
+#   pulseLength /= 4096                     # 12 bits of resolution
+#   print "%d us per bit" % pulseLength
+#   pulse *= 1000
+#   pulse /= pulseLength
+#   pwm.setPWM(channel, 0, pulse)
+
+pwm.setPWMFreq(60)                        # Set frequency to 60 Hz
+while (True):
+  # Change speed of continuous servo on channel O
+  pwm.setPWM(0, 0, servoMin)
+  time.sleep(1)
+  pwm.setPWM(0, 0, servoMax)
+  time.sleep(1)

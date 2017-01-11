222019bdb32ecf6cddfe17c1cf121d534c18b797
Noah Berman <noah@noahberman.org>
2017-01-11 13:13:55 -0700

    add full brew sleep time

--- servo_controller.py
+++ servo_controller.py
@@ -23,6 +23,6 @@ pwm.setPWMFreq(60)                        # Set frequency to 60 Hz
 
 while (True):
   pwm.setPWM(0, 0, servoMax)
-  time.sleep(1)
+  time.sleep(300)
   pwm.setPWM(0, 0, servoMin)
   exit(0)
